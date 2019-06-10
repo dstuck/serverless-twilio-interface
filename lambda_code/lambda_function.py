0e300494f1172ce66fa50a872610d511c69888b6
dstuck <david.e.stuck@gmail.com>
2019-06-10 08:43:46 -0500

    Clean up xml construction thanks to a snippet from @awelzel

--- lambda_code/lambda_function.py
+++ lambda_code/lambda_function.py
@@ -1,12 +1,13 @@
+import io
 import urllib
+import xml.etree.ElementTree as ET
 
 
 def lambda_handler(event, context):
     print("Received event: " + str(event))
     message_text = event['Body']
     text_response = urllib.parse.unquote_plus(handle_text_message(message_text))
-    return '<?xml version=\"1.0\" encoding=\"UTF-8\"?>'\
-           '<Response><Message>{}</Message></Response>'.format(text_response)
+    return _create_xml_for_message(text_response)
 
 
 def handle_text_message(message_text):
@@ -16,3 +17,14 @@ def handle_text_message(message_text):
     """
     response = "{} to you too".format(message_text)
     return response
+
+
+def _create_xml_for_message(message):
+    resp = ET.Element('Response')
+    msg = ET.SubElement(resp, 'Message')
+    msg.text = message
+
+    tree = ET.ElementTree(resp)
+    buf = io.BytesIO()
+    tree.write(buf, xml_declaration=True, encoding='utf-8')
+    return buf.getvalue().decode()
